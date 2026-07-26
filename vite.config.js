aa00af73a434750ec7d70a151bbfe9d72e5f48ad
Juan Maurelia <juan.maurelia@gmail.comgit config --global user.name Juan>
2026-07-26 19:29:18 -0400

    feat: add @vitejs/plugin-legacy to support older browser environments

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['chrome >= 38', 'safari >= 9'],
+    }),
+  ],
 })
